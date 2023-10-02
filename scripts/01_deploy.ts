b48240f2b9098ac0fd2a9b54d021565c3eb17c8b
zeeshanhanif <zeeshanhanif@gmail.com>
2023-10-02 23:10:08 +0500

    Added script for deployment

---
+++ scripts/01_deploy.ts
@@ -0,0 +1,22 @@
+import { ethers, network } from "hardhat";
+import { NusicClips, NusicClips__factory } from "../typechain-types";
+
+async function main() {
+
+  const [owner, addr1] = await ethers.getSigners();
+  console.log("Network = ",network.name);
+
+  const NusicClips:NusicClips__factory = await ethers.getContractFactory("NusicClips");
+  const nusicClips:NusicClips = await NusicClips.deploy("Clips1","NC1");
+  await nusicClips.deployed();
+
+  console.log("NusicClips deployed to:", nusicClips.address);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
